0444e0091a60199eb148dc440607197c2c4548bb
mohamed <mothman198@outlook.com>
2022-08-05 03:24:36 +0400

    fix visualize_an_aug.py and demonstrations notebook

--- arcmentations/visualize_an_aug.py
+++ arcmentations/visualize_an_aug.py
@@ -5,18 +5,23 @@ from arc.utils.dataset import get_riddles
 from arcmentations import functional
 from arcmentations.augmentations.color import RandomColor
 from arcmentations.augmentations.helpers import same_aug_for_all_pairs_helper
-from arcmentations.augmentations.spatial import DirectionTypeRandomDouble, RandomDoubleInputBoard
+from arcmentations.augmentations.spatial import DirectionTypeRandomDouble, RandomDoubleInputBoard, RandomReflect, RandomRotate
 from arcmentations.vis_helpers import plot_task
 from arcmentations.augmentations import RandomCropInputAndOuput
-
+from torchvision import transforms
 if __name__ == "__main__":
     train_riddles = get_riddles(["training"])
     riddle = train_riddles[1]
-    # riddle.train = RandomCropInputAndOuput(p=1, same_aug_for_all_pairs=True, possible_num_cols_to_crop=[1], possible_num_rows_to_crop=[0])(riddle.train)
-    plot_task(riddle)
-    params = RandomColor.get_params(seed=random.random(), include_0=False)
-
-    riddle.train = same_aug_for_all_pairs_helper(riddle.train, functional.permute_color, True, params_in=params)
+    transform = transforms.Compose([
+        transforms.RandomOrder([
+            RandomCropInputAndOuput(1, same_aug_for_all_pairs=True),
+            RandomDoubleInputBoard(1, same_aug_for_all_pairs=True),
+        ]),
+	    RandomColor(1, same_aug_for_all_pairs=True,include_0=False),
+        RandomRotate(0.5, True),
+        RandomReflect(0.5, True),
+    ])
+    riddle.train = transform(riddle.train)
     
     plot_task(riddle)
     time.sleep(100000)
